454ca563920a7a342afea7fdecc98820fa3b048e
Felipe Castillo <jfcastillo1204@gmail.com>
2019-10-13 11:09:18 -0500

    Intentando conectar varios clientes

--- Prueba/src/hilos/HiloConexion.java
+++ Prueba/src/hilos/HiloConexion.java
@@ -1,14 +1,29 @@
 package hilos;
 
+import java.net.ServerSocket;
+
 import modelo.Servidor;
 
 public class HiloConexion extends Thread {
 	
 	private Servidor server;
+	private ServerSocket serverSocket;
 	
 
-	public HiloConexion() {
-		
+	
+	public HiloConexion(Servidor server, ServerSocket serverSocket) {
+		super();
+		this.server = server;
+		this.serverSocket = serverSocket;
+	}
+
+
+
+	@Override
+	public void run() {
+		while(true) {
+			
+		}
 	}
 
 	
